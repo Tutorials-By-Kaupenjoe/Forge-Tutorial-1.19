e5e6c7c3cdd55b3ba8bfb8fe7f8642b7bce67ec8
Kaupenjoe <N.Kaupenjohann@gmail.com>
2022-06-16 22:41:58 +0200

    tooltips

--- src/main/java/net/kaupenjoe/tutorialmod/item/custom/EightBallItem.java
+++ src/main/java/net/kaupenjoe/tutorialmod/item/custom/EightBallItem.java
@@ -1,5 +1,7 @@
 package net.kaupenjoe.tutorialmod.item.custom;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
@@ -7,7 +9,11 @@ import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class EightBallItem extends Item {
     public EightBallItem(Properties properties) {
@@ -22,6 +28,17 @@ public class EightBallItem extends Item {
         }
 
         return super.use(level, player, hand);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
+        if(Screen.hasShiftDown()) {
+            components.add(Component.literal("Right click to get a random number!").withStyle(ChatFormatting.AQUA));
+        } else {
+            components.add(Component.literal("Press SHIFT for more info").withStyle(ChatFormatting.YELLOW));
+        }
+
+        super.appendHoverText(stack, level, components, flag);
     }
 
     private void outputRandomNumber(Player player) {
