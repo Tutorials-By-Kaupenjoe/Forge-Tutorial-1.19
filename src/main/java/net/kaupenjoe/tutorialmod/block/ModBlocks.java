9acb8de45a13c8be6a099cc6c4378b601554ae3b
Kaupenjoe <N.Kaupenjohann@gmail.com>
2022-06-24 16:48:11 +0200

    blockstates

--- src/main/java/net/kaupenjoe/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/kaupenjoe/tutorialmod/block/ModBlocks.java
@@ -2,6 +2,7 @@ package net.kaupenjoe.tutorialmod.block;
 
 import net.kaupenjoe.tutorialmod.TutorialMod;
 import net.kaupenjoe.tutorialmod.block.custom.JumpyBlock;
+import net.kaupenjoe.tutorialmod.block.custom.ZirconLampBlock;
 import net.kaupenjoe.tutorialmod.item.ModCreativeModeTab;
 import net.kaupenjoe.tutorialmod.item.ModItems;
 import net.minecraft.util.valueproviders.UniformInt;
@@ -39,6 +40,13 @@ public class ModBlocks {
     public static final RegistryObject<Block> JUMPY_BLOCK = registerBlock("jumpy_block",
             () -> new JumpyBlock(BlockBehaviour.Properties.of(Material.STONE)
                     .strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTab.TUTORIAL_TAB);
+
+    public static final RegistryObject<Block> ZIRCON_LAMP = registerBlock("zircon_lamp",
+            () -> new ZirconLampBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(6f).requiresCorrectToolForDrops()
+                    .lightLevel(state -> state.getValue(ZirconLampBlock.LIT) ? 15 : 0)), ModCreativeModeTab.TUTORIAL_TAB);
+
+
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
---
+++ src/main/java/net/kaupenjoe/tutorialmod/block/custom/ZirconLampBlock.java
@@ -0,0 +1,35 @@
+package net.kaupenjoe.tutorialmod.block.custom;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.phys.BlockHitResult;
+
+public class ZirconLampBlock extends Block {
+    public static final BooleanProperty LIT = BooleanProperty.create("lit");
+
+    public ZirconLampBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level level, BlockPos blockPos,
+                                 Player player, InteractionHand hand, BlockHitResult result) {
+        if(!level.isClientSide() && hand == InteractionHand.MAIN_HAND) {
+            level.setBlock(blockPos, state.cycle(LIT),3);
+        }
+
+        return super.use(state, level, blockPos, player, hand, result);
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(LIT);
+    }
+}
