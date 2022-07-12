ec35f65c6b90fa49fb77babcc848d4a5148ab5a0
Kaupenjoe <N.Kaupenjohann@gmail.com>
2022-07-12 19:00:47 +0200

    keybindings

---
+++ src/main/java/net/kaupenjoe/tutorialmod/util/KeyBinding.java
@@ -0,0 +1,14 @@
+package net.kaupenjoe.tutorialmod.util;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+import net.minecraftforge.client.settings.KeyConflictContext;
+import org.lwjgl.glfw.GLFW;
+
+public class KeyBinding {
+    public static final String KEY_CATEGORY_TUTORIAL = "key.category.tutorialmod.tutorial";
+    public static final String KEY_DRINK_WATER = "key.tutorialmod.drink_water";
+
+    public static final KeyMapping DRINKING_KEY = new KeyMapping(KEY_DRINK_WATER, KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_O, KEY_CATEGORY_TUTORIAL);
+}
