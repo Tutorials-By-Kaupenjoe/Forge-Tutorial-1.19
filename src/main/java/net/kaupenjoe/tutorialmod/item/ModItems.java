7b9375df74b8fafe27f6a7f2929ab9cb33de058e
Kaupenjoe <N.Kaupenjohann@gmail.com>
2022-07-30 22:53:48 +0200

    custom item model

--- src/main/java/net/kaupenjoe/tutorialmod/item/ModItems.java
+++ src/main/java/net/kaupenjoe/tutorialmod/item/ModItems.java
@@ -5,10 +5,7 @@ import net.kaupenjoe.tutorialmod.block.ModBlocks;
 import net.kaupenjoe.tutorialmod.fluid.ModFluids;
 import net.kaupenjoe.tutorialmod.item.custom.EightBallItem;
 import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BucketItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemNameBlockItem;
-import net.minecraft.world.item.Items;
+import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -38,6 +35,10 @@ public class ModItems {
     public static final RegistryObject<Item> SOAP_WATER_BUCKET = ITEMS.register("soap_water_bucket",
             () -> new BucketItem(ModFluids.SOURCE_SOAP_WATER,
                     new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB).craftRemainder(Items.BUCKET).stacksTo(1)));
+
+    public static final RegistryObject<Item> KAUPENSWORD = ITEMS.register("kaupensword",
+            () -> new SwordItem(Tiers.DIAMOND, 10, 5f,
+                    new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB).stacksTo(1)));
 
 
 
